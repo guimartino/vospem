4f484ce2fbcd81e720ad037539cfcdff6bff6b01
Gui <gui.martino@hotmail.com>
2017-12-11 15:53:49 -0200

    ci Changed to POST

--- chat/f/subscribe.php
+++ chat/f/subscribe.php
@@ -4,6 +4,17 @@
   //https://graph.facebook.com/ID/subscribed_apps?access_token=PAGE_TOKEN
   $id = $_POST['page_id'];
   $token = $_POST['page_token'];
-  $t = file_get_contents("https://graph.facebook.com/$id/subscribed_apps?access_token=$token");
+  $t = httpPost("https://graph.facebook.com/$id/subscribed_apps?access_token=$token");
   echo $t;
   //header('Location: pages.php');
+
+  function httpPost($url, $data = array())
+  {
+      $curl = curl_init($url);
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      $response = curl_exec($curl);
+      curl_close($curl);
+      return $response;
+  }
