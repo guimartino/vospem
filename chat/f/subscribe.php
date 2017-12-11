69cf28cbb302a6c9802cacb9bfe21a25e96f6b7a
Gui <gui.martino@hotmail.com>
2017-12-11 15:52:07 -0200

    ci

--- chat/f/subscribe.php
+++ chat/f/subscribe.php
@@ -4,6 +4,6 @@
   //https://graph.facebook.com/ID/subscribed_apps?access_token=PAGE_TOKEN
   $id = $_POST['page_id'];
   $token = $_POST['page_token'];
-  file_get_contents("https://graph.facebook.com/$id/subscribed_apps?access_token=$token");
-
-  header('Location: pages.php');
+  $t = file_get_contents("https://graph.facebook.com/$id/subscribed_apps?access_token=$token");
+  echo $t;
+  //header('Location: pages.php');
