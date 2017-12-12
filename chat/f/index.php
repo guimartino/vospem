46d4d4c433b37db092119155e4b5346c31802a74
Gui <gui.martino@hotmail.com>
2017-12-12 15:07:23 -0200

    a

--- chat/f/index.php
+++ chat/f/index.php
@@ -1,5 +1,8 @@
 <?php
 include("../../include/data.php");
-
-header('Location:  '.$domain.'/chat/f/login.php');
+if(!isset($_SESSION['fb_access_token'])){
+  header('Location:  '.$domain.'/chat/f/login.php');
+}else{
+  header('Location:  '.$domain.'/chat/f/pages.php');
+}
  ?>
