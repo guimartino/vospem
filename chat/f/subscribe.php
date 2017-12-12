081b78f5c11363b2822864ba06a682c964271456
Gui <gui.martino@hotmail.com>
2017-12-12 16:31:53 -0200

    a

--- chat/f/subscribe.php
+++ chat/f/subscribe.php
@@ -17,20 +17,15 @@
 
   if($r->success == 1){
     echo "Sucesso";
+    //salvar no banco
+
+    //salvar no banco o LOG
   }else{
     echo "Ocorreu um erro!";
+
+    //salvar no banco o LOG
   }
-  //echo $t;
-  // salvar no banco ID da pagina e Token
-
-
-  //header('Location: pages.php');
-
-
-
-
-
-
+  
       /*
         Inscrever pagina no app:
             https://graph.facebook.com/530951440254946/subscribed_apps?access_token=EAAR9nHZBpogcBAMghZBAQ2sIqVGYJITZCSUvaXb7RCwawDhQZBnLyiKUNS1C9IP6TE6PbWMbS0PJ831EdW89bAbW7yjVUq1bs9lngfFkga60PzJCr04Nm4qEeqR6ZAlLb1WpohevhnceZATuIKdUIZBJ7ZAJZCX9ZBtFZAvq6EZAW1wKgtCBYMZCopzNw
