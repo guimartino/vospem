4f9a6f8cf77fd0fc538301cc010abbf8c0695b66
Gui <gui.martino@hotmail.com>
2017-12-12 16:21:35 -0200

    c

--- chat/f/subscribe.php
+++ chat/f/subscribe.php
@@ -14,34 +14,7 @@
   //$r = fixJSON($r, "}", 'right', false);
   //$r = $r[0];
   ///echo "<br>Lado: ".$r."<br>";
-  $r = (string) $r;
-  $r = jsonDecode($r, true);
-  switch (json_last_error()) {
-        case JSON_ERROR_NONE:
-            echo ' - No errors';
-        break;
-        case JSON_ERROR_DEPTH:
-            echo ' - Maximum stack depth exceeded';
-        break;
-        case JSON_ERROR_STATE_MISMATCH:
-            echo ' - Underflow or the modes mismatch';
-        break;
-        case JSON_ERROR_CTRL_CHAR:
-            echo ' - Unexpected control character found';
-        break;
-        case JSON_ERROR_SYNTAX:
-            echo ' - Syntax error, malformed JSON';
-        break;
-        case JSON_ERROR_UTF8:
-            echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
-        break;
-        default:
-            echo ' - Unknown error';
-        break;
-    }
-
-  //$r = strlen($r);
-  $r = "aaaa".$r."aaa";
+  
   print_r($r);
   //echo $t;
   // salvar no banco ID da pagina e Token
