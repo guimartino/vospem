7f0ea656cefc8971dfdd43f16ed1846fae6d13c0
Guilherme Martino <gui.martino@hotmail.com>
2017-12-17 13:10:28 -0200

    lockuser v2

---
+++ chat/f/lockuserv2.php
@@ -0,0 +1,89 @@
+<?php
+include("../../include/data.php");
+$start = date('Y-m-d H:i:s');
+checkLogin();
+  if(isset($_POST['page_id'])){
+    $page_id = $_POST['page_id'];
+    $page_token = $_POST['page_token'];
+  }else{
+    header("Location: pages.php");
+  }
+
+
+/* PHP SDK v5.0.0 */
+/* make the API call */
+  $users = getUsersMessagePage($fb, $page_id, $page_token);
+  //print_r($users);
+  //graph.facebook.com/1428386897275804/picture?type=large
+  foreach ($users as $user_id => $user_name) {
+      $user_image = getUserImage($user_id);
+      $class = "yes";
+      $user_blocked = getUserLocked($page_id, $user_id);
+
+      ?>
+        <div style="" id="div_<?=$user_id;?>" onClick="changeclass('div_<?=$user_id?>')" class="<?=$user_blocked;?>">
+          <table>
+            <tr>
+              <td>
+                <img src="<?=$user_image?>"style="width:80px;">
+              </td>
+              <td>
+                <span><?=$user_name?></span>
+              </td>
+            </tr>
+          </table>
+        </div>
+      <?php
+  }
+  echo "<br><br>";
+
+/* handle the result */
+
+
+
+
+?>
+<script>
+    function changeclass(divName) {
+
+    var div = document.getElementById(divName);
+    var user_id = divName.split("_")[1];
+    if(div.className == "yes"){
+      div.className = "no";
+      url = "../../include/actions.php?action=LockUnlockUser&value=1&user_id="+user_id+"&page_id=<?=$page_id?>";
+      $.get(url, function(data, status){
+        alert("Usuario bloqueado com sucesso");
+        console.log(url);
+      });
+    }else{
+      div.className = "yes";
+      url = "../../include/actions.php?action=LockUnlockUser&value=0&user_id="+user_id+"&page_id=<?=$page_id?>";
+      $.get(url, function(data, status){
+        alert("Usuario desbloqueado");
+        console.log(url);
+      });
+    }
+
+    }
+</script>
+<style>
+  .yes{
+    border:2px solid #007001; background:#60c961; min-height: 100px; margin-bottom: 10px; border-radius:5px;
+    padding:15px;
+    width: 50%;
+  }
+  .yes span{
+      color: #007001;
+      font-weight: bolder;
+    }
+
+  .no{
+    color: #7f0000;
+    font-weight: bolder;
+  }
+  .no{
+    border:2px solid #7f0000; background:#e06b6b; min-height: 100px; margin-bottom: 10px; border-radius:5px;
+    padding:15px;
+    width: 50%;
+  }
+</style>
--- chat/f/messages.php
+++ chat/f/messages.php
@@ -24,7 +24,7 @@ $graphEdge = $response->getGraphEdge();
 $array = $graphEdge->asArray();
 //print_r($array);
 //echo "".$_POST['page_token'];
-print_r($array);
+//print_r($array);
 foreach ($array as $key => $value) {
   echo "Id " . $key . ": " . $value['id']."<br>";
   $conversas = file_get_contents('https://graph.facebook.com/'.$value['id'].'/?fields=can_reply,former_participants,id,is_subscribed,link,message_count,participants,name,senders,subject&access_token='.$_POST['page_token']);
@@ -33,22 +33,22 @@ foreach ($array as $key => $value) {
     if($p['id']!=$_POST['page_id']){
       echo "Nome: ".$p['name']."<br>"."Id: ".$p['id'];
     }
-    try {
-      $response = $fb->get(
-        '/'.$p['id'].'?fields=name,age_range,ids_for_pages',
-        ''.$_POST['page_token']
-      );
-    } catch(Facebook\Exceptions\FacebookResponseException $e) {
-      echo 'Graph returned an error: ' . $e->getMessage();
-      exit;
-    } catch(Facebook\Exceptions\FacebookSDKException $e) {
-      echo 'Facebook SDK returned an error: ' . $e->getMessage();
-      exit;
-    }
-
-    $graphEdge = $response->getGraphEdge();
-    $array = $graphEdge->asArray();
-    print_r($array);
+    // try {
+    //   $response = $fb->get(
+    //     '/'.$p['id'].'?fields=name,age_range,ids_for_pages',
+    //     ''.$_POST['page_token']
+    //   );
+    // } catch(Facebook\Exceptions\FacebookResponseException $e) {
+    //   echo 'Graph returned an error: ' . $e->getMessage();
+    //   //exit;
+    // } catch(Facebook\Exceptions\FacebookSDKException $e) {
+    //   echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    //   //exit;
+    // }
+    //
+    // $graphEdge = $response->getGraphEdge();
+    // $array = $graphEdge->asArray();
+    // print_r($array);
 
 
   }
