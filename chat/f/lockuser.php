5009eb8f51e2333f7a7a9614099b650d543343f8
Guilherme Martino <gui.martino@hotmail.com>
2017-12-17 01:08:33 -0200

    print

--- chat/f/lockuser.php
+++ chat/f/lockuser.php
@@ -6,7 +6,7 @@ checkLogin();
 /* PHP SDK v5.0.0 */
 /* make the API call */
   $users = getUsersMessagePage($fb, $_POST['page_id'], $_POST['page_token']);
-
+  print_r($users);
   echo "<br><br>";
 
 /* handle the result */
