dee6d200be7d642040119a4e24df20b0704bee4c
Guilherme Martino <gui.martino@hotmail.com>
2017-12-17 01:20:33 -0200

    using session

--- chat/f/lockuser.php
+++ chat/f/lockuser.php
@@ -2,16 +2,30 @@
 include("../../include/data.php");
 $start = date('Y-m-d H:i:s');
 checkLogin();
+  if(isset($_SESSION['lockuser_page_id']) && $_SESSION['lockuser_page_id'] != $_POST['page_id']){
+    $_SESSION['lockuser_page_id'] = $_POST['page_id'];
+  }elseif(!isset($_SESSION['lockuser_page_id'])){
+    $_SESSION['lockuser_page_id'] != $_POST['page_id'];
+  }
+ if(isset($_SESSION['lockuser_page_token']) && $_SESSION['lockuser_page_token'] != $_POST['page_token']){
+    $_SESSION['lockuser_page_token'] = $_POST['page_token'];
+  }elseif(!isset($_SESSION['lockuser_page_token'])){
+    $_SESSION['lockuser_page_token'] != $_POST['page_token'];
+  }
+
+    $page_id = $_SESSION['lockuser_page_id'];
+    $page_token = $_SESSION['page_token'];
 
 /* PHP SDK v5.0.0 */
 /* make the API call */
-  $users = getUsersMessagePage($fb, $_POST['page_id'], $_POST['page_token']);
+  $users = getUsersMessagePage($fb, $page_id, $page_token);
   print_r($users);
   //graph.facebook.com/1428386897275804/picture?type=large
-  foreach ($users as $key => $value) {
+  foreach ($users as $user_id => $user_name) {
+      $user_imagem = getUserImage($user_id);
       ?>
-        <div style="border:2px solid #007001; background:#60c961; width:50%; height: 100px;">
-          aas
+        <div style="border:2px solid #007001; background:#60c961; width:50%; height: 100px; margin-bottom: 10px; border-radius:5px;">
+
         </div>
       <?php
   }
