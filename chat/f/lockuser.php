9bd05f02b32f5f2055e1a2defec729127276e9d8
Guilherme Martino <gui.martino@hotmail.com>
2017-12-17 00:52:47 -0200

    Added button

--- chat/f/lockuser.php
+++ chat/f/lockuser.php
@@ -6,7 +6,7 @@ checkLogin();
 /* PHP SDK v5.0.0 */
 /* make the API call */
   $users = getUsersMessagePage($_POST['page_id'], $_POST['page_token']);
-
+  
   echo "<br><br>";
 }
 /* handle the result */
