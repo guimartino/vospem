01540d1587786ac76938b63bde10aeca917d1fba
Guilherme Martino <gui.martino@hotmail.com>
2017-12-17 03:56:42 -0200

    print

--- chat/f/messages.php
+++ chat/f/messages.php
@@ -24,6 +24,7 @@ $graphEdge = $response->getGraphEdge();
 $array = $graphEdge->asArray();
 //print_r($array);
 //echo "".$_POST['page_token'];
+print_r($array);
 foreach ($array as $key => $value) {
   echo "Id " . $key . ": " . $value['id']."<br>";
   $conversas = file_get_contents('https://graph.facebook.com/'.$value['id'].'/?fields=can_reply,former_participants,id,is_subscribed,link,message_count,participants,name,senders,subject&access_token='.$_POST['page_token']);
