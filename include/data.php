c016d47c5bc9276627b70e39df96ffd755d31f53
Gui <gui.martino@hotmail.com>
2017-12-18 15:51:18 -0200

    page token

--- include/data.php
+++ include/data.php
@@ -13,11 +13,12 @@
 
   $domain = "https://vospem.com/";
   $app_id = "1264011017036295";
+  $api_version = 'v2.11';
 
   $fb = new \Facebook\Facebook([
     'app_id' => $app_id,
     'app_secret' => 'c1642f39152539b59460933e65c5f0d0',
-    'default_graph_version' => 'v2.11',
+    'default_graph_version' => $api_version,
     //'default_access_token' => '{access-token}', // optional
   ]);
 
